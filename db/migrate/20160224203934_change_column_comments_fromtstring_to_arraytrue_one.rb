eff0893935f139e7022a2cccbf645ec270ef74be
Snita Bharth <snitabharth@hotmail.com>
2016-02-25 16:39:58 +0000

    fixing migration

--- db/migrate/20160224203934_change_column_comments_fromtstring_to_arraytrue_one.rb
+++ db/migrate/20160224203934_change_column_comments_fromtstring_to_arraytrue_one.rb
@@ -1,10 +1,8 @@
-      class ChangeColumnCommentsFromtstringToArraytrue
-@
- < ActiveRecord::Migration
-        def change
-          change_table(:videos) do |t|
-            t.column :comments, :string, array: true
-            # Other column alterations here
-          end
-        end
-      end
+class ChangeColumnCommentsFromtstringToArraytrue < ActiveRecord::Migration
+  def change
+    change_table(:videos) do |t|
+      t.column :comments, :string, array: true
+      # Other column alterations here
+    end
+  end
+end
